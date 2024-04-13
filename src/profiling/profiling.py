4e080ab431be2659bfeb83de39503d43c8e08fd8
xhavlij00 <xhavlij00@stud.fit.vutbr.cz>
2024-04-13 16:58:53 +0200

    Edited calculation of standard deviation - more compact
    removed unnecessary variables so the calculation is more compact
    also edited how the result is printed out - now prints out just the number
    and sums are not calculated separately

--- src/profiling/profiling.py
+++ src/profiling/profiling.py
@@ -12,21 +12,15 @@ def profiler_function():
         input.append(float(line))
     
     sum1 = 0
-    for i in input:
-        sum1 = mathlib.add(sum1, i)
-    xlined = mathlib.div(sum1, len(input)) # x with line
-
     sum2 = 0
     for i in input:
-        sum2 = mathlib.add(sum2, mathlib.pow(i, 2)) # sum of all x_{i}^{2}
-    Nx2 = mathlib.mul(len(input), mathlib.pow(xlined, 2)) # n * (x with line)^2
-    secondhalf = mathlib.sub(sum2, Nx2) # sum of all x_{i}^{2} - n * (x with line)^2
+        sum1 = mathlib.add(sum1, i)
+        sum2 = mathlib.add(sum2, mathlib.pow(i, 2))
 
-    firsthalf = mathlib.div(1, mathlib.sub(len(input), 1)) # (1 / (n - 1))
-    result = mathlib.mul(firsthalf, secondhalf)
-    result = mathlib.root(result, 2)
+    # deviation = sqrt((1/(n-1)) * (sum(x_{i}^{2}) - n * (sum(x_{i})/n)^{2}))
+    deviation = mathlib.root(mathlib.mul(mathlib.div(1, mathlib.sub(len(input), 1)), mathlib.sub(sum2, mathlib.mul(len(input), mathlib.pow(mathlib.div(sum1, len(input)), 2)))), 2)
     
-    print("Deviation =", result)
+    print(deviation)
 
 # use with python3 profiling.py < input_$number.txt (gives us flat profile output)
 # e.g:
