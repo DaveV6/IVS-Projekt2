4be32358d1c1243a5b439bac523a63b0b22ef626
xhavlij00 <xhavlij00@stud.fit.vutbr.cz>
2024-04-17 14:56:31 +0200

    Fixed issue in profiling.py, Translated plan and profiling report into English
    because we use english in commits and in doxygen documentation, we wanted to unify everything.
    in profiling.py, fixed the reading so it can be separated by any whitespace and now it can load unlimited number of inputs

--- src/profiling/profiling.py
+++ src/profiling/profiling.py
@@ -5,14 +5,15 @@
 *   Description:    This file serves for profiling of our mathlib - calculates standard deviation
 *                   from the formula given in the assignment and prints out the output as a flat profile.
 *
-*   Last change:    13.04.2024
+*   Last change:    17.04.2024
 *   Date:           13.04.2024
 *   Author:         Jakub Havlík (xhavlij00)
 **************************************************************************************************'''
-##
-# @file profiling.py
-# @author Jakub Havlík (xhavlij00)
-# @brief Mathlib profiling - calculates standard deviation from given formula
+"""
+@file profiling.py
+@author Jakub Havlík (xhavlij00)
+@brief Mathlib profiling - calculates standard deviation from given formula
+"""
 
 
 import sys
@@ -23,22 +24,21 @@ sys.path.append('../')
 import mathlib
 
 
-##
-# @brief reads all lines from stdin and calculates the standard deviation
-
+"""
+@brief reads all lines from stdin and calculates the standard deviation
+"""
 def profiler_function():
-    input = []
-    for line in sys.stdin:
-        input.append(float(line))
-    
     sum1 = 0
     sum2 = 0
-    for i in input:
-        sum1 = mathlib.add(sum1, i)
-        sum2 = mathlib.add(sum2, mathlib.pow(i, 2))
+    i = 0
+    for line in sys.stdin:
+        for num in line.split():
+            sum1 = mathlib.add(sum1, float(num))
+            sum2 = mathlib.add(sum2, mathlib.pow(float(num), 2))
+            i = i + 1
 
     # deviation = sqrt((1/(n-1)) * (sum(x_{i}^{2}) - n * (sum(x_{i})/n)^{2}))
-    deviation = mathlib.root(mathlib.mul(mathlib.div(1, mathlib.sub(len(input), 1)), mathlib.sub(sum2, mathlib.mul(len(input), mathlib.pow(mathlib.div(sum1, len(input)), 2)))), 2)
+    deviation = mathlib.root(mathlib.mul(mathlib.div(1, mathlib.sub(i, 1)), mathlib.sub(sum2, mathlib.mul(i, mathlib.pow(mathlib.div(sum1, i), 2)))), 2)
     
     print(deviation)
 
@@ -47,5 +47,3 @@ def profiler_function():
 # python3 profiling.py < input_10.txt > output_profiling.txt
 # python3 profiling.py < input_1000.txt >> output_profiling.txt (appends to the file)
 cProfile.run('profiler_function()', sort='time')
-
-'''***END OF FILE PROFILING.PY***'''
